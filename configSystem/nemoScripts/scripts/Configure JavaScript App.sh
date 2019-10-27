283ebecee552a198c6852cc6bfc772a1fd422fa8
LucasFDutra <lucasfelipedutra@gamil.com>
2019-10-27 19:27:59 -0300

    add babel-eslint

--- configSystem/nemoScripts/scripts/Configure JavaScript App.sh
+++ configSystem/nemoScripts/scripts/Configure JavaScript App.sh
@@ -5,6 +5,7 @@ yarn add eslint-config-airbnb -D
 yarn add eslint-plugin-import -D
 yarn add eslint-plugin-jsx-a11y -D
 yarn add eslint-plugin-react -D
+yarn add babel-eslint
 
 wget https://raw.githubusercontent.com/LucasFDutra/Ambiente-React-E-React-native-Com-ESlint-Airbnb/master/.editorconfig
 wget https://raw.githubusercontent.com/LucasFDutra/Ambiente-React-E-React-native-Com-ESlint-Airbnb/master/.eslintrc.json
