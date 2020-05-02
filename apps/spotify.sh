46f4104496d97658c4809b5f4345bd5c5f88dbf0
LucasFDutra <lucasfelipedutra@gamil.com>
2020-05-01 21:01:38 -0300

    mudando spotify para snap

--- apps/spotify.sh
+++ apps/spotify.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt update
-sudo apt-get install spotify-client -y
+sudo snap install spotify
